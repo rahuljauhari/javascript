04e11a57eb5bbc40a7cbd388f148247d9160b744
rahuljauhari <jauhari.rahul@gmail.com>
2019-10-24 12:49:50 +0530

    commit mmmmm

---
+++ notes.js
@@ -0,0 +1,26 @@
+
+const notes = ['Note 1','Note 2','Note 3']
+
+//Remove Item from Array
+console.log(notes.pop())
+
+
+//Push New Note 
+notes.push('My New Note') //push new note
+console.log(notes)
+
+//length of array
+console.log(notes.length)
+
+//Print the Position 0 value in the array
+console.log(notes[0])
+
+//print the value in the array @ position 2
+console.log(notes[notes.length-1])
+
+//print the position in the array @ position 1
+console.log(notes[notes.length-2])
+
+//print the position in the array @ position 0
+console.log(notes[notes.length-3])
+
---
+++ notesshift.js
@@ -0,0 +1,16 @@
+const notes = ['Note 1','Note 2','Note 3']
+console.log(notes)
+
+//Remove Item from Array
+//console.log(notes.pop())
+
+console.log(notes.length)
+
+//Push New Note 
+notes.push('My New Note') //push new note
+console.log(notes[notes.length-1])
+
+//console.log(notes.shift())
+
+//length of array
+//console.log(notes.length)
