832996b8f7e3c07657be117b1757526ee50711de
rahuljauhari <jauhari.rahul@gmail.com>
2019-10-24 10:48:21 +0530

    new commit

---
+++ const1.js
@@ -0,0 +1,8 @@
+
+const person = {
+    age: 27
+}
+
+person.age = 25
+//person = {}
+console.log(person)
---
+++ grade-calculator.js
@@ -0,0 +1,19 @@
+
+let gradeCalc = function (score,totalScore) {
+    let percentage = (score/totalScore) * 100
+    let letterGrade = ''
+
+    if ( percentage > 90 ){letterGrade ='A'}
+     else if ( percentage < 90 && percentage > 80){ letterGrade = 'B' }
+     else if (percentage <80 && percentage >60) {letterGrade = 'C'}
+     else if (percentage < 60 && percentage > 50) {letterGrade = 'D'}
+     else {letterGrade = 'E'}
+
+return `You got a ${letterGrade} and ${percentage}%`
+}
+
+console.log(gradeCalc(50,100))
+console.log(gradeCalc(65,100))
+console.log(gradeCalc(75,100))
+console.log(gradeCalc(85,100))
+console.log(gradeCalc(95,100))
